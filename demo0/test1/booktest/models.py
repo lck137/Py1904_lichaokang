26b608e3a7ba7053011a4c2d20af6a081679ceb3
lck <1372199437@qq.com>
2019-07-02 19:06:38 +0800

    创建了一个项目， 创建了一个应用

---
+++ demo0/test1/booktest/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+#MVT 中的M 数据模块
+#ORM对象
+# Create your models here.
+
+class Bookinfo(models.Model):
+    title=models.CharField(max_length=20)
+    pub_time=models.DateTimeField(auto_now=True)
+
+class Heroinfo(models.Model):
+    name=models.CharField(max_length=20)
+    gender=models.BooleanField(default=True)
+    content=models.CharField(max_length=100)
+    book=models.ForeignKey(Bookinfo,on_delete=models.CASCADE)
+
+
+
