10b7eef5cc9971cce164f098f6c0a2ba4e562a44
lck <1372199437@qq.com>
2019-07-08 19:47:49 +0800

    使用cookie方法登录

--- demo0/test4/polls/urls.py
+++ demo0/test4/polls/urls.py
@@ -9,6 +9,8 @@ urlpatterns=[
     url(r"^$",views.index,name="index"),
     url(r"^detail/(\d+)/$",views.detail,name="detail"),
     url(r"^result/(\d+)/$",views.result,name="result"),
+    url(r"^plogin/$",views.plogin,name='plogin'),
+    url(r"^ploginout/$",views.ploginout,name='ploginout'),
 
 ]
 
--- demo0/test4/polls/views.py
+++ demo0/test4/polls/views.py
@@ -4,10 +4,28 @@ from django.shortcuts import render,reverse,redirect
 from .models import Question,Choice
 
 
-def index(request):
-    questions=Question.objects.all()
-    return render(request,"polls/index.html",{"questions":questions})
+#装饰器
 
+def checklogin(func):
+    def check(request,*args):
+        username=request.COOKIES.get('username')
+        if username:
+            return func(request,*args)
+        else:
+            return redirect(reverse("polls:plogin"))
+    return check
+
+@checklogin
+def index(request):
+    username = request.COOKIES.get('username')
+    if username != None:
+        questions=Question.objects.all()
+        return render(request,"polls/index.html",{"questions":questions,'username':username})
+    else:
+        return redirect(reverse("polls:plogin"))
+
+
+@checklogin
 def detail(request,id):
     question=Question.objects.get(pk=id)
     if request.method == 'GET':
@@ -19,12 +37,26 @@ def detail(request,id):
         option.save()
 
         return redirect(reverse("polls:result",args=(id,)))
-
+@checklogin
 def result(request,id):
     question=Question.objects.get(pk=id)
     return render(request,"polls/result.html",{'question':question})
 
 
+#登录
+def plogin(request):
+    if request.method == "GET":
+        return render(request,"polls/plogin.html",{})
+    elif request.method == "POST":
+        response=redirect(reverse("polls:index"))
+        response.set_cookie("username",request.POST.get('username'))
+        return response
+#退出
+def ploginout(request):
+    # 1.cookie方法
+    res=redirect(reverse("polls:plogin"))
+    res.delete_cookie('username')
+    return res
 
 
 
